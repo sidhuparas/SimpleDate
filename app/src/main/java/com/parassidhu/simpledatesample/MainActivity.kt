0aee1ba42824511609abb52eef6017fde4cba2e4
sidhuparas <sidhuparas1234@gmail.com>
2019-08-11 16:26:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/parassidhu/simpledatesample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.parassidhu.simpledatesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.parassidhu.simpledate.SimpleDate
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ simpledate/src/main/java/com/parassidhu/simpledate/SimpleDate.java
@@ -0,0 +1,8 @@
+package com.parassidhu.simpledate;
+
+public class SimpleDate {
+
+    public static void print() {
+        System.out.println("Hey");
+    }
+}
